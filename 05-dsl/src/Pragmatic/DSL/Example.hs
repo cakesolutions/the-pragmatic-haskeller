c8aead0920b2ce68a8639d3404138c05bdd723de
Alfredo Di Napoli <alfredod@cakesolutions.net>
2013-02-28 21:44:05 +0000

    Added parsing for quantity

--- 05-dsl/src/Pragmatic/DSL/Example.hs
+++ 05-dsl/src/Pragmatic/DSL/Example.hs
@@ -3,7 +3,8 @@
 module Pragmatic.DSL.Parser where
 
 import Text.ParserCombinators.Parsec
-import Control.Applicative
+import Text.Parsec hiding (try)
+import Control.Applicative hiding ((<|>), optional)
 
 -- Shameless copy of the "Parsing Stuff in
 -- Haskell" talk.
@@ -21,4 +22,36 @@ alwaysTrue = pure True
 -- (*>) :: Applicative f => f a -> f b -> f b
 boolTrue :: Parser Bool
 boolTrue = (string "true") *> alwaysTrue
+
+data Quantity = Quantity
+    { qt :: Int
+    , ms :: Maybe String } deriving Show
+
+(<||>) :: Parser a -> Parser a -> Parser a
+p <||> q = (try p) <|> q
+
+-- Here we are saying, try to match one between
+-- gr and ml, in you can't default to Nothing.
+-- The trick is using pure
+-- pure :: a -> f a
+measure :: Parser (Maybe String)
+measure = (string "gr" *> (pure $ Just "gr"))
+     <||> (string "ml" *> (pure $ Just "ml"))
+     <||> (pure Nothing)
+
+parseMeasure = parse measure "pmeasure"
+
+ws :: Parser String
+ws = many1 (oneOf " ")
+
+int :: (Integral a, Read a) => Parser a
+int = read <$> (many1 digit)
+
+quantity :: Parser Quantity
+quantity = do
+    qt <- int <* ws
+    ms <- measure
+    return $ Quantity qt ms
+    
+
 
