b36159f7e20a48c81dcb9f7037fc9bcc1da36410
Alfredo Di Napoli <alfredod@cakesolutions.net>
2013-03-01 13:51:23 +0000

    Almost parsed Ingredients, needs to support of as optional

--- 05-dsl/src/Pragmatic/DSL/Example.hs
+++ 05-dsl/src/Pragmatic/DSL/Example.hs
@@ -3,8 +3,9 @@
 module Pragmatic.DSL.Parser where
 
 import Text.ParserCombinators.Parsec
+import Pragmatic.Types
 import Text.Parsec hiding (try)
-import Control.Applicative hiding ((<|>), optional)
+import Control.Applicative hiding ((<|>), optional, many)
 
 -- Shameless copy of the "Parsing Stuff in
 -- Haskell" talk.
@@ -28,30 +29,41 @@ data Quantity = Quantity
     , ms :: Maybe String } deriving Show
 
 (<||>) :: Parser a -> Parser a -> Parser a
-p <||> q = (try p) <|> q
+p <||> q = try p <|> q
 
 -- Here we are saying, try to match one between
 -- gr and ml, in you can't default to Nothing.
 -- The trick is using pure
 -- pure :: a -> f a
-measure :: Parser (Maybe String)
-measure = (string "gr" *> (pure $ Just "gr"))
+measureP :: Parser (Maybe String)
+measureP = (string "gr" *> (pure $ Just "gr"))
      <||> (string "ml" *> (pure $ Just "ml"))
      <||> (pure Nothing)
 
-parseMeasure = parse measure "pmeasure"
 
 ws :: Parser String
 ws = many1 (oneOf " ")
 
 int :: (Integral a, Read a) => Parser a
-int = read <$> (many1 digit)
+int = read <$> many1 digit
 
-quantity :: Parser Quantity
-quantity = do
-    qt <- int <* ws
-    ms <- measure
+stringLike :: Parser String
+stringLike = char '"' *> many (noneOf ['"']) <* char '"'
+
+-- A parser combinator which skips whitespaces
+lexeme :: Parser a -> Parser a
+lexeme p = p <* ws
+
+quantityP :: Parser Quantity
+quantityP = do
+    qt <- lexeme int
+    ms <- measureP
     return $ Quantity qt ms
-    
 
-
+ingredient :: Parser Ingredient
+ingredient = do
+    qt <- lexeme int
+    ms <- lexeme measureP
+    lexeme (string "of")
+    name <- stringLike
+    return $ Ingredient name qt ms
